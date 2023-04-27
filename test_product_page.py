e96b97c6c4ccecd593678acdef57d9f8fd6a5bf6
Oksana Moskvina <o.moskvina@bi.zone>
2023-04-27 14:37:17 +0300

    parametrization for product_page

--- test_product_page.py
+++ test_product_page.py
@@ -1,17 +1,21 @@
-from .pages.main_page import MainPage
+# from .pages.main_page import MainPage
 from .pages.product_page import ProductPage
+import pytest
 
-# ЗДЕСЬ Тесты (http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear).
-# Сообщение о том, что товар добавлен в корзину. Название товара в сообщении должно совпадать с тем товаром, который вы действительно добавили.
-# Сообщение со стоимостью корзины. Стоимость корзины совпадает с ценой товара. 
-
-
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)                          # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()
     page.add_product_to_basket()
     page.should_be_same_product_name()
-    page.should_be_same_product_price()  
-
-
+    page.should_be_same_product_price()
+      
